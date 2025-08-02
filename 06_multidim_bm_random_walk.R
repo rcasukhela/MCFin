84bb6b2dd76cfd4e547bc6807078cf558820c33d
Rohan Casukhela <43920866+rcasukhela@users.noreply.github.com>
2025-08-02 01:07:03 +0000

    Cov Matrix allowed to be less than full rank
    Some adjustments I made to my construction of the Overall Covariance Matrix so that it was allowed to be symmetric positive semidefinite. This generalizes to Glasserman's 3.15 on page 91.

--- 06_multidim_bm_random_walk.R
+++ 06_multidim_bm_random_walk.R
@@ -4,7 +4,7 @@ The goal is to generate a multi-dimensional Brownian Motion.
 We will create a d-dimensional BM.
 "
 require(MASS) # import mvrnorm()
-n = 100 # number of entries
+n = 1000 # number of entries
 d = 3 # dimension of B.M.
 mu = matrix(runif(d, -5, 5), ncol= 1)
 
@@ -18,9 +18,13 @@ This generates a d-dimensional integer-valued random matrix
 that is SYMMETRIC POSITIVE-DEFINITE!!! Remember, this needs to be
 such a matrix because we need a covariance matrix.
 "
-B_T = matrix(round(rnorm(n=d*d, 0, 10), 0), nrow=d)
+nrows = sample.int(d, 1)
+ncols = d
+problem_dim = max(nrows, ncols)
+
+B_T = matrix(round(rnorm(n=nrows*ncols, 0, 10), 0), nrow=nrows, ncol=ncols)
 B = t(B_T)
-Sigma = B %*% t(B) + diag(d)*1e-6
+Sigma = B %*% t(B) + diag(problem_dim)*1e-6
 
 # This is how to check that the matrix is symm pos-def.
 sum(eigen(Sigma)$values > 0) == d
@@ -37,7 +41,7 @@ X = matrix(
 
 X[ , 1] = rep(0, d)
 for ( i in 1:(n-1) ){
-  Z = matrix(mvrnorm(n=1, rep(0, d), diag(d)), ncol=1)
+  Z = matrix(mvrnorm(n=1, rep(0, dim(B)[2]), diag(dim(B)[2])), ncol=1)
   X[ , i+1] = X[ , i]  +  mu * (i+1 - i)  +  sqrt(i+1 - i) * B %*% Z
 }
 # Just putting the (i+1 - i) in for pedagogical purposes.
